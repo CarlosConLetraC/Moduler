8486a81f4759e4ca82b57c962a1a9d2de79595a1
Carlos <111033132+CarlosConLetraC@users.noreply.github.com>
2026-02-10 20:24:31 -0600

    Update configurarluarocks.sh

--- configurarluarocks.sh
+++ configurarluarocks.sh
@@ -4,18 +4,32 @@ set -e
 BASE_PATH=$PWD
 LUAROCKS_VERSION="3.13.0"
 
-dependencies=( "luajit" "libluajit-5.1-dev" "wget" "make" "gcc" "libssl-dev" "build-essential" "pkg-config" "libssl-dev" "zlib1g-dev" "ca-certificates" "libssh2-1" "libssh2-1-dev" )
+dependencies=(
+    "luajit"
+    "libluajit-5.1-dev"
+    "wget"
+    "make"
+    "gcc"
+    "build-essential"
+    "pkg-config"
+    "libssl-dev"
+    "zlib1g-dev"
+    "ca-certificates"
+    "libssh2-1"
+    "libssh2-1-dev"
+    "git"
+)
 
 sudo apt-get update
 for dep in "${dependencies[@]}"; do
-    if ! dpkg -l | grep -q $dep; then
-        echo "$dep no esta instalado. Instalando..."
-        sudo apt-get install -y $dep
+    if ! dpkg -s "$dep" &>/dev/null; then
+        echo "$dep no está instalado. Instalando..."
+        sudo apt-get install -y "$dep"
     fi
 done
 
 cd /tmp
-wget https://luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz
+wget -q https://luarocks.org/releases/luarocks-$LUAROCKS_VERSION.tar.gz
 tar zxpf luarocks-$LUAROCKS_VERSION.tar.gz
 cd luarocks-$LUAROCKS_VERSION
 ./configure && make && sudo make install
@@ -24,30 +38,15 @@ sudo luarocks install luasocket
 sudo luarocks install luasec
 
 
-if [ -f "/usr/local/lib/libluajit-5.1.so" ] || [ -f "/usr/local/lib/libluajit-5.1.so.2" ]; then
-    LUAJIT_LIB_DIR="/usr/local/lib"
-    LUAJIT_INCLUDE_DIR="/usr/local/include/luajit-2.1"
-elif [ -f "/usr/lib/libluajit-5.1.so" ] || [ -f "/usr/lib/libluajit-5.1.so.2" ]; then
-    LUAJIT_LIB_DIR="/usr/lib"
-    LUAJIT_INCLUDE_DIR="/usr/include/luajit-2.1"
-else
-    echo "No se encontró LuaJIT en /usr/local/lib ni en /usr/lib"
-    exit 1
-fi
-
-echo "LuaJIT detectado en: $LUAJIT_LIB_DIR"
-
 cd /tmp
-if [ -d lua-ssh/ ]; then
-    rm -rf lua-ssh/
-fi
-
+rm -rf lua-ssh
 git clone https://github.com/esno/lua-ssh.git
 cd lua-ssh/src
 
-gcc -O2 -fPIC -I$LUAJIT_INCLUDE_DIR -c ssh.c -o ssh.o
-gcc -shared -o ssh.so ssh.o -L$LUAJIT_LIB_DIR -lluajit-5.1 -lssh2
+gcc -O2 -fPIC -I/usr/include/luajit-2.1 -c ssh.c -o ssh.o
+gcc -shared -o ssh.so ssh.o -lluajit-5.1 -lssh2
 
-cp -p ssh.so $BASE_PATH/import/Linux/
+mkdir -p "$BASE_PATH/import/Linux/"
+cp -p ssh.so "$BASE_PATH/import/Linux/"
 
 echo "OK"
