6e544f2a4bedf0e93c522be533fa65846188bbdf
Carlos <111033132+CarlosConLetraC@users.noreply.github.com>
2026-04-08 00:25:51 -0600

    Delete .program.test.lua

--- .program.test.lua
+++
@@ -1,9 +0,0 @@
-do return end; import("Table", "system", "csv")
-
-local f = system.curldownload("https://www.openml.org/data/get_csv/16826755/phpMYEkMl", true)
-local t = csv.read(f)
-
---print(t)
---for k, v in t.iter do
---	print(k, v)
---end
